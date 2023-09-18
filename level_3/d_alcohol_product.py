edabdb8a7618d8946cd4f7f0ff25fe3e69f836be
System256 <system256@ya.ru>
2023-09-18 23:07:11 +0300

    d done

--- level_3/d_alcohol_product.py
+++ level_3/d_alcohol_product.py
@@ -4,30 +4,35 @@
 алкоголь нельзя продавать с 5 утра до 11 вечера
 
 Задания:
-    1. Переопределите методов is_available в классе AlcoholProduct с использованием super()
+    1. Переопределите метод is_available в классе AlcoholProduct с использованием super()
     2. is_available у AlcoholProduct должен возвращать False если сейчас часы между 5 утра и 11 вечера.
        Для определения текущего часа можно использовать datetime.now().hour
     3. Создайте экземпляр класса AlcoholProduct и проверьте, можно ли сейчас продавать алкоголь.
 """
 from datetime import datetime
+from decimal import Decimal
 
 
 class Product:
-    def __init__(self, title, price, stock_quantity):
+    def __init__(self, title: str, price: Decimal, stock_quantity: int) -> None:
         self.title = title
         self.price = price
         self.stock_quantity = stock_quantity
 
-    def get_discounted_price(self, discount_percentage):
+    def get_discounted_price(self, discount_percentage) -> Decimal:
         return self.price * (1 - discount_percentage / 100)
 
-    def is_available(self):
+    def is_available(self) -> bool:
         return self.stock_quantity > 0
 
 
 class AlcoholProduct(Product):
-    pass  # код писать тут
+    def is_available(self) -> bool:
+        # С not думаю более наглядно, либо можно 5 > datetime.now().hour > 23
+        # Можно включительно 5 и 23 часа, тогда проставим <=, но в условии задачи не было
+        return super().is_available() and not (5 < datetime.now().hour < 23)
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    alcohol_product = AlcoholProduct(title='beer', price=300, stock_quantity=12)
+    print(alcohol_product.is_available())
