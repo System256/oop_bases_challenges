ee7ef970e04e2650d159480141fb2bd51052ee76
System256 <roman.palcev@gmail.com>
2023-10-09 20:42:32 +0300

    level_6 done

--- level_6/a_log_products.py
+++ level_6/a_log_products.py
@@ -1,5 +1,5 @@
 """
-У нас есть различные типы классы для различных типов продуктов. Но мы ничего не знает о том что происходит, когда мы вызываем
+У нас есть различные типы классы для различных типов продуктов. Но мы ничего не знаем о том что происходит, когда мы вызываем
 эти методы, хотелось бы простейшего логирования
 
 Задания:
@@ -11,32 +11,52 @@
 
 
 class Product:
-    def __init__(self, title: str, price: float):
+    def __init__(self, title: str, price: float) -> None:
         self.title = title
         self.price = price
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f'Product {self.title} with price {self.price}'
+    
+
+class PrintLoggerMixin():
+    def log(self, message: str) -> str:
+        print(f'[LOG]: {message}')
 
 
-class PremiumProduct(Product):
-    def increase_price(self):
+class PremiumProduct(PrintLoggerMixin, Product):
+    def increase_price(self) -> None:
+        self.log('Logging def increase_price')
         self.price *= 1.2
 
-    def get_info(self):
+    def get_info(self) -> str:
+        self.log('Logging def get_info')
         base_info = super().get_info()
         return f'{base_info} (Premium)'
 
 
-class DiscountedProduct(Product):
-    def increase_price(self):
+class DiscountedProduct(PrintLoggerMixin, Product):
+    def increase_price(self) -> None:
+        self.log('Logging def increase_price')
         self.price /= 1.2
 
-    def get_info(self):
+    def get_info(self) -> str:
+        self.log('Logging def get_info')
         base_info = super().get_info()
         return f'{base_info} (Discounted)'
-
+    
 
 if __name__ == '__main__':
-    pass
+    print('-' * 60)
 
+    premium_product = PremiumProduct(title='Banana', price=150)
+    premium_product.increase_price()
+    print(premium_product.get_info())
+
+    print('-' * 60)
+
+    discounted_product = DiscountedProduct(title='Kiwi', price=50)
+    discounted_product.increase_price()
+    print(discounted_product.get_info())
+
+    print('-' * 60)
--- level_6/b_developer.py
+++ level_6/b_developer.py
@@ -1,7 +1,7 @@
 """
 
 Задания:
-    1. Создайте класс Developer, который будет наследоваться от класса Employee и класса SuperAdminMixin.
+    1. Создайте класс Developer, который будет наследоваться от класса ItDepartmentEmployee и класса SuperAdminMixin.
     2. Переопределите у класса Developer метод __init__ таким образом, чтобы он на вход принимал еще и язык программирования.
     3. Переопределите метод get_info у класса Developer таким образом, чтобы там выводился еще и язык программирования.
     4. Вызовите у экземпляра класса Developer все возможные методы.
@@ -9,35 +9,49 @@
 
 
 class Employee:
-    def __init__(self, name: str, surname: str, age: int, salary: float):
+    def __init__(self, name: str, surname: str, age: int, salary: float) -> None:
         self.name = name
         self.surname = surname
         self.age = age
         self.salary = salary
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f'{self.name} with salary {self.salary}'
 
 
 class ItDepartmentEmployee(Employee):
-    def __init__(self, name: str, surname: str, age: int, salary: float):
+    def __init__(self, name: str, surname: str, age: int, salary: float) -> None:
         super().__init__(name, surname, age, salary)
         self.salary *= 2
 
 
 class AdminMixin:
-    def increase_salary(self, employee: Employee, amount: float):
+    def increase_salary(self, employee: Employee, amount: float) -> None:
         employee.salary += amount
 
 
 class SuperAdminMixin(AdminMixin):
-    def decrease_salary(self, employee: Employee, amount: float):
+    def decrease_salary(self, employee: Employee, amount: float) -> None:
         employee.salary -= amount
 
 
-# код писать тут
+class Developer(SuperAdminMixin, ItDepartmentEmployee):
+    def __init__(self, name: str, surname: str, age: int, salary: float, programming_language: str) -> None:
+        super().__init__(name, surname, age, salary)
+        self.programming_language = programming_language
+
+    def get_info(self) -> str:
+        return f'{self.name} with salary {self.salary}, programming language: {self.programming_language}'
 
 
 if __name__ == '__main__':
-    pass  # код писать тут
+    print('-' * 60)
 
+    developer = Developer(name='Joe', surname='Weaver', age=40, salary=150000.8, programming_language='Python')
+    print(developer.get_info())
+    developer.increase_salary(employee=developer, amount=10000)
+    print(developer.get_info())
+    developer.decrease_salary(employee=developer, amount=50000)
+    print(developer.get_info())
+
+    print('-' * 60)
